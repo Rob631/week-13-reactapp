29687d268c863f4339c1305fcc2cbfdbd8972f86
Roberto Diaz <robertodiaz381@yahoo.com>
2022-10-31 10:04:00 -0500

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="/" className="site-title">Diaz Logistics</a>
+        <ul>
+            <li>
+                <a href="/">Planes</a>
+            </li>
+            <li>
+                <a href="/">Routes</a>
+            </li>
+        </ul>
+    </nav>
+}
